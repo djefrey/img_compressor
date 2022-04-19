0683d454ac08a6065219746d01effefca92cf6b8
Jeffrey Winkler <jeffrey.winkler@epitech.eu>
2022-04-19 15:25:41 +0200

    feat: choose random pixels + color distance

--- app/Main.hs
+++ app/Main.hs
@@ -5,15 +5,19 @@ import Lib
 import System.IO
 import System.Exit
 import System.Environment ( getArgs )
+import System.Random ( StdGen, getStdRandom, randomR )
 
 import Options.Applicative
 import Data.Semigroup ((<>))
 import Data.Int ( Int8 )
 import Text.Read ( readMaybe )
 
+type Point = (Int, Int)
+type Color = (Int8, Int8, Int8)
+
 data Pixel = Pixel {
-    getPos :: (Int, Int),
-    getColor :: (Int8, Int8, Int8)
+    getPos :: Point,
+    getColor :: Color
 } deriving (Show)
 
 data Opts = Opts {
@@ -21,6 +25,13 @@ data Opts = Opts {
     getLimit :: Float,
     getPath :: String
 }
+
+getRed   :: Color -> Int8
+getGreen :: Color -> Int8
+getBlue  :: Color -> Int8
+getRed   (r,_,_) = r
+getGreen (_,g,_) = g
+getBlue  (_,_,b) = b
 
 optsParser :: ParserInfo Opts
 optsParser = info (options) (fullDesc)
@@ -49,6 +60,7 @@ lineToPixel line =
         replaceCommas c = case c of
             ',' -> ' '
             _ -> c
+
         parseEntry :: [String] -> Pixel
         parseEntry (x:y:r:g:b:[]) =
             (Pixel (read x :: Int, read y :: Int)
@@ -57,6 +69,23 @@ lineToPixel line =
 parseImg :: [String] -> [Pixel]
 parseImg [] = []
 parseImg (line:xs) = (lineToPixel line):(parseImg xs)
+
+removeAt :: [a] -> Int -> [a]
+removeAt list i = (take i list) ++ (drop (i + 1) list)
+
+chooseRandomPixels :: Int -> [Pixel] -> IO [Pixel]
+chooseRandomPixels 0 _ = return []
+chooseRandomPixels n list = do
+    randIndex <- getStdRandom $ randomR (0, (length list) - 1)
+    points <- chooseRandomPixels (n - 1) (removeAt list randIndex)
+    return ((list !! randIndex):points)
+
+colorDist :: Color -> Color -> Float
+colorDist start end =
+    let r = fromIntegral((getRed end) - (getRed start))
+        g = fromIntegral((getGreen end) - (getGreen start))
+        b = fromIntegral((getBlue end) - (getBlue start))
+    in r ** 2 + g ** 2 + b ** 2
 
 printPixels :: [Pixel] -> IO ()
 printPixels [] = return ()
@@ -67,5 +96,7 @@ printPixels (pixel:xs) = do
 main :: IO ()
 main = do
     opts <- execParser optsParser
-    imgFile <- readFile (getPath opts)
-    printPixels (parseImg (lines imgFile))
+    file <- readFile (getPath opts)
+    let img = parseImg (lines file)
+    pixels <- chooseRandomPixels (getColors opts) img
+    putStrLn $ show $ (colorDist (getColor $ (pixels !! 0)) (getColor $ (pixels !! 1)))
