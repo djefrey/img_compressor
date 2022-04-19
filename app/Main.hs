cbd5ca9a7b8363eeeefca457987a418a2e2e6f2d
Jeffrey Winkler <jeffrey.winkler@epitech.eu>
2022-04-19 13:43:38 +0200

    feat: file parser

--- app/Main.hs
+++ app/Main.hs
@@ -8,11 +8,18 @@ import System.Environment ( getArgs )
 
 import Options.Applicative
 import Data.Semigroup ((<>))
+import Data.Int ( Int8 )
+import Text.Read ( readMaybe )
+
+data Pixel = Pixel {
+    getPos :: (Int, Int),
+    getColor :: (Int8, Int8, Int8)
+} deriving (Show)
 
 data Opts = Opts {
-    colors :: Int,
-    limit :: Float,
-    path :: String
+    getColors :: Int,
+    getLimit :: Float,
+    getPath :: String
 }
 
 optsParser :: ParserInfo Opts
@@ -33,7 +40,32 @@ optsParser = info (options) (fullDesc)
                 <> metavar "F"
                 <> help "path to the file containing the colors of the pixels")
 
+lineToPixel :: String -> Pixel
+lineToPixel line =
+    let values = words (map replaceCommas (filter (`notElem` "()") line))
+    in parseEntry values
+    where
+        replaceCommas :: Char -> Char
+        replaceCommas c = case c of
+            ',' -> ' '
+            _ -> c
+        parseEntry :: [String] -> Pixel
+        parseEntry (x:y:r:g:b:[]) =
+            (Pixel (read x :: Int, read y :: Int)
+            (read r :: Int8, read g :: Int8, read b :: Int8))
+
+parseImg :: [String] -> [Pixel]
+parseImg [] = []
+parseImg (line:xs) = (lineToPixel line):(parseImg xs)
+
+printPixels :: [Pixel] -> IO ()
+printPixels [] = return ()
+printPixels (pixel:xs) = do
+    putStrLn (show pixel)
+    printPixels xs
+
 main :: IO ()
 main = do
     opts <- execParser optsParser
-    return ()
+    imgFile <- readFile (getPath opts)
+    printPixels (parseImg (lines imgFile))
